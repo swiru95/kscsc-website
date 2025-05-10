fdea79d6bd9793bdcd29a52e42af23f8e8880a14
Krzysztof Swidrak <24733214+swiru95@users.noreply.github.com>
2025-05-10 21:04:12 +0200

    Imrpoved mail send - CSP

--- contact/js/image.js
+++ contact/js/image.js
@@ -3,3 +3,9 @@ document.querySelectorAll('img[data-link]').forEach(img => {
     window.location.href = img.dataset.link;
   });
 });
+
+document.querySelectorAll('div[data-mailto]').forEach(div => {
+  div.addEventListener('click', () => {
+    window.location.href = div.dataset.mailto;
+  });
+});
